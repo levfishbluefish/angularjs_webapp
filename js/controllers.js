af77981c8aa760f3d289286002eb45aec99afb7f
catfistori <cat.fistori@gmail.com>
2015-09-10 15:06:47 -0700

    Added angular-route functionality

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,16 @@
-var myApp = angular.module('myApp', []);
+var artistControllers = angular.module('artistControllers', []);
 
-myApp.controller('MyController', ['$scope', "$http", function($scope, $http) {
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
-   $scope.artists = data;
-   $scope.artistOrder = 'name';
+    $scope.artists = data;
+    $scope.artistOrder = 'name';
+  });
+}]);
 
- })
+artistControllers.controller('DetailsController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.artists = data;
+    $scope.whichItem = $routeParams.itemId;
+  });
 }]);
 
